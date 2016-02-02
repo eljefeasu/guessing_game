1b3e336b06a634d39fb7cf96f8efefb72fefa69e
Jefferson Neel <jefferson.neel@gmail.com>
2016-02-02 18:56:02 -0500

    Changed variable names and added counter for number of guesses remaining

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,24 +1,20 @@
-def get_user_input
-  print "What do you think the integer is? "
-  return gets.chomp
-end
-
-def is_duplicate?(single_user_input, user_input)
-  if user_input.include?(single_user_input)
+def is_duplicate?(user_input, user_input_array)
+  if user_input_array.include?(user_input)
     puts "I think you might need to go lie down. You entered that number already :P "
   end
 end
 
-def higher_or_lower?(computer_number, user_input)
-  if user_input.last.to_i > computer_number.to_i
+def higher_or_lower?(computer_number, user_input_array)
+  if user_input_array.last.to_i > computer_number.to_i
     puts "That is not the number. Your guess was too high."
   end
-  if user_input.last.to_i < computer_number.to_i
+  if user_input_array.last.to_i < computer_number.to_i
     puts "That is not the number. Your guess was too low."
   end
 end
 
-user_input = []
+counter = 5
+user_input_array = []
 rand_array=*(0..100)
 rand_array.shuffle!
 computer_number = rand_array[0]
@@ -26,18 +22,25 @@ puts computer_number
 puts "I'm thinking of an integer between 1 and 100."
 
 (1..5).each do |num|
-  single_user_input = get_user_input
-  is_duplicate?(single_user_input, user_input)
-  user_input << single_user_input
+  if counter > 1
+    puts "You have #{counter} guesses left."
+  else
+    puts "You have 1 guess left."
+  end
+  print "What do you think the integer is? "
+  user_input = gets.chomp
+  is_duplicate?(user_input, user_input_array)
+  user_input_array << user_input
 
-  if user_input[num - 1].to_i == computer_number
+  if user_input_array[num - 1].to_i == computer_number
     puts "That's it! You got it, my number was: #{computer_number}"
     break
   end
 
-  higher_or_lower?(computer_number, user_input)
+  higher_or_lower?(computer_number, user_input_array)
+  counter -= 1
 
-  if num == 5
+  if counter == 0
     puts "I'm sorry, but you were not able to guess my number. The number is #{computer_number}."
   end
 end
