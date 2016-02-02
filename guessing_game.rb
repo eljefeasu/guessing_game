6ad4429b6d7a552ee1adf6afc7f74a039f1f9fae
Jefferson Neel <jefferson.neel@gmail.com>
2016-02-02 14:27:48 -0500

    After 5 incorrect guesses the program ends

--- guessing_game.rb
+++ guessing_game.rb
@@ -3,14 +3,25 @@ def get_user_input
   return gets.chomp
 end
 
+def higher_or_lower?(computer_number, user_input)
+  
+end
+
+user_input = []
 computer_number = rand(1..100)
 puts computer_number
 puts "I'm thinking of an integer between 1 and 100."
 
-5.times do
-  user_input = get_user_input
-  if user_input.to_i == computer_number
+(1..5).each do |number|
+  user_input << get_user_input
+  if user_input[number - 1].to_i == computer_number
     puts "That's it! You got it, my number was: #{computer_number}"
     break
   end
+  if number == 5
+    puts "I'm sorry you were not able to guess my number"
+  end
+
+  higher_or_lower?(computer_number, user_input)
+
 end
